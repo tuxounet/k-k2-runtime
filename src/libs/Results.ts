c269fbcdf4890650d973787baaa543b7fb0c1592
Christophe Tiraoui <tuxounet@gmail.com>
2023-02-07 20:52:00 +0100

    fail api result

--- src/libs/Results.ts
+++ src/libs/Results.ts
@@ -1,10 +1,21 @@
 import { IApiResult } from "../types/api/IApiResult";
+export enum ResultStatus {
+  OK = 200,
+  NOT_FOUND = 404,
+}
 
 export class Results {
-  successApiResult(message: string, body?: unknown): IApiResult {
+  successApiResult(body?: unknown): IApiResult {
     return {
       statusCode: 200,
-      body: body !== undefined ? JSON.stringify(body) : message,
+      body: body !== undefined ? JSON.stringify(body) : "",
+    };
+  }
+
+  successFailureResult(code: ResultStatus, message?: string): IApiResult {
+    return {
+      statusCode: code,
+      body: JSON.stringify({ reason: message }),
     };
   }
 }
